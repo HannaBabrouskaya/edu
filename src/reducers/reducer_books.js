76e43a9fba304e6d0669ea89baa4c5c4c30715e7
Hanna <anka.grodno@gmail.com>
2017-11-29 19:09:14 +0300

    [change] redux start

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: "lalalala", pages: 111},
+        {title: "Castle", pages: 22},
+        {title: "Gogogo", pages: 20},
+        {title: "Js", pages: 1},
+    ]
+}
